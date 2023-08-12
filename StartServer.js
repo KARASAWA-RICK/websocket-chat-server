081aec66f175d5a1e8bd3f4c2a7c49b2abfb0977
Rick <takuei.rick@gmail.com>
2023-08-12 20:23:42 +0800

    第二次提交

--- StartServer.js
+++ StartServer.js
@@ -1,22 +1,15 @@
 //初始化服务器 + 注册服务
 
-
-
 //全局数据存储
-const Global = require('./Global')
+const Global = require("./Global");
 //配置服务器 + 预配置session
-const netbus = require("./netbus.js")
+const netbus = require("./netbus.js");
 //服务管理
-const serviceMgr = require("./serviceMgr.js")
+const serviceMgr = require("./serviceMgr.js");
 //聊天室服务
-const chatroomService = require("./chatroomService")
-
-
-
-
+const chatroomService = require("./chatroomService");
 
 //初始化服务器
-netbus.initServer("10.0.12.138", 6085)
+netbus.initServer("127.0.0.1", 6085);
 //注册聊天室服务
-serviceMgr.registService(Global.STYPE.Chatroom, chatroomService)
-
+serviceMgr.registService(Global.STYPE.Chatroom, chatroomService);
